92fa2f57582e1a074702c7b6bb42ede6c0a93b52
Andrew Ellis <astoellis@gmail.com>
2015-08-19 21:55:38 -0400

    Adds animate.css and small JavaScript file. TODO: Remove necessity of JavaScript entirely, or commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+	$('.portfolio-item').on('mouseenter', function(e){
+		$(this).children('.desc').show().addClass('animated slideInUp');
+	});
+	$('.portfolio-item').on('mouseleave', function(e){
+		$(this).children('.desc').hide().removeClass('animated slideInUp');
+	});
+});
