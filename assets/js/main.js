b0faa14bcbef0ac0c80644bd88cda2f3a8fdccf9
Andrew Ellis <astoellis@gmail.com>
2015-08-21 10:41:55 -0400

    Adds smaller images for all subjects, should help loadtime.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$('.portfolio-item').on('mouseenter', function(e){
-		$(this).children('.desc').show().addClass('animated slideInUp');
-	});
-	$('.portfolio-item').on('mouseleave', function(e){
-		$(this).children('.desc').hide().removeClass('animated slideInUp');
-	});
+  $('.portfolio-item').on('mouseenter', function(e) {
+    $(this).children('.desc').show().addClass('animated slideInUp');
+  });
+  $('.portfolio-item').on('mouseleave', function(e) {
+    $(this).children('.desc').hide().removeClass('animated slideInUp');
+  });
 });
